64fb445b001e0f5bdf9d6764bd97d249570a3306
johltn <j.luttun@laposte.net>
2021-02-26 11:48:34 +0100

    Use MVD rules to get psets/qsets

--- application/checks/check_bSDD.py
+++ application/checks/check_bSDD.py
@@ -2,18 +2,19 @@ import requests
 import json
 import pprint
 import ifcopenshell
+from ifcopenshell.mvd import mvd
 
+ifc_fn = "./ifc-python-parser/files/AC20-Institute-Var-2.ifc"
+ifc_fn = "./ifc-python-parser/files/test_file.ifc"
 
-ifc_fn = "./ifc-python-parser/files/AC20-FZK-Haus.ifc"
 ifc_file = ifcopenshell.open(ifc_fn)
 
 mvd_fn = "./ifcopenshell/mvd/mvd_examples/officials/ReferenceView_V1-2.mvdxml"
 
-
 def get_xset_rule(mvd_fn, ifc_type, pset_or_qset):
     mvd_concept_roots = ifcopenshell.mvd.concept_root.parse(mvd_fn)
     for concept_root in mvd_concept_roots:
-        if concept_root.entity == ifc_type:
+        if concept_root.entity == ifc_type :
             for c in concept_root.concepts():
                 print('concept name ', c.name)
   
@@ -29,12 +30,20 @@ def get_xset_rule(mvd_fn, ifc_type, pset_or_qset):
 
 pset = 'Property Sets for Objects'
 qset = 'Quantity Sets'
-print(get_xset_rule(mvd_fn, 'IfcWall', qset))
-rule_tree = get_xset_rule(mvd_fn, 'IfcWall', qset)
-for result in mvd.extract_data(rule_tree, ifc_file.by_type("IfcWall")[0]):
-    # print(result)
-    for k,v in result.items():
-        print(k, v)
+# print(get_xset_rule(mvd_fn, 'IfcWall', qset))
+
+
+# rule_tree = get_xset_rule(mvd_fn, 'IfcWall', qset)
+# for result in mvd.extract_data(rule_tree, ifc_file.by_type("IfcWall")[0]):
+#     # print(result)
+#     for k,v in result.items():
+#         print(k, v)
+#     print()
+
+
+
+
+
 
 def get_ifc_types(file):
     return {e.is_a() for e in file.by_type("IfcBuildingElement")}
@@ -45,17 +54,27 @@ built_request = 'https://bs-dd-api-prototype.azurewebsites.net/api/Classificatio
 ifc_bsdd = {}
 
 for t in types:
-    r = requests.get(built_request+ t.lower())
-    ifc_bsdd[t] = json.loads(r.text)
+    if t == 'IfcWindow':
+        r = requests.get(built_request+ t.lower())
+        # ifc_bsdd[t] = json.loads(r.text)
+        classification_result = json.loads(r.text)
 
-class_test = ifc_bsdd['IfcWindow']
-print(json.dumps(class_test, indent=4, sort_keys=True))
-classification_properties = class_test["classificationProperties"]
-print(len(classification_properties))
 
-for cp in classification_properties:
-    print(cp)
- 
+        rule_tree = get_xset_rule(mvd_fn, t, pset)
+        print(rule_tree)
+        for result in mvd.extract_data(rule_tree, ifc_file.by_type(t)[0]):
+        # print(result)
+            for k,v in result.items():
+                print(k, v)
+            
+        
+        if len(classification_result['classificationProperties']):
+            for prop in classification_result['classificationProperties']:
+                print(prop['propertySet'])
+                print(prop['name'])
+                print(prop['dataType'])
+               
+      
 
 
 
