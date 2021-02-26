4e35866b8291f656ff164e1cf26c9718f42340e6
johltn <j.luttun@laposte.net>
2021-02-26 12:28:30 +0100

    Intermediate commit: use anytree to visualize rule trees

--- application/checks/check_bSDD.py
+++ application/checks/check_bSDD.py
@@ -4,8 +4,12 @@ import pprint
 import ifcopenshell
 from ifcopenshell.mvd import mvd
 
+import ast
+
+from anytree import Node, RenderTree
+
 ifc_fn = "./ifc-python-parser/files/AC20-Institute-Var-2.ifc"
-ifc_fn = "./ifc-python-parser/files/test_file.ifc"
+#ifc_fn = "./ifc-python-parser/files/test_file.ifc"
 
 ifc_file = ifcopenshell.open(ifc_fn)
 
@@ -20,6 +24,7 @@ def get_xset_rule(mvd_fn, ifc_type, pset_or_qset):
   
                 if c.name == pset_or_qset:
                     print("ct name", c.template().name)
+                    print("ct dir", dir(c.template()))
                     print(c.concept_node)
                     print(c.rules())
                     print(c.template().rules)
@@ -28,25 +33,40 @@ def get_xset_rule(mvd_fn, ifc_type, pset_or_qset):
                         if r.attribute == "IsDefinedBy":
                             return r
 
-pset = 'Property Sets for Objects'
-qset = 'Quantity Sets'
-# print(get_xset_rule(mvd_fn, 'IfcWall', qset))
-
-
-# rule_tree = get_xset_rule(mvd_fn, 'IfcWall', qset)
-# for result in mvd.extract_data(rule_tree, ifc_file.by_type("IfcWall")[0]):
-#     # print(result)
-#     for k,v in result.items():
-#         print(k, v)
-#     print()
-
-
-
-
-
-
 def get_ifc_types(file):
     return {e.is_a() for e in file.by_type("IfcBuildingElement")}
+
+
+def create_tree(rulepoint, parent_node=None):
+    #Function to create a tree of the Rules of the ConceptTemplates
+    if parent_node:
+        racine = Node(rulepoint,id=rulepoint.attribute, parent=parent_node)
+    else:
+        racine = Node(rulepoint, id=rulepoint.attribute)
+
+    for node in rulepoint.nodes:
+
+        create_tree(node, racine)
+    return racine
+
+
+pset = 'Property Sets for Objects'
+qset = 'Quantity Sets'
+
+
+rule_tree = get_xset_rule(mvd_fn, 'IfcWall', qset)
+mytree = create_tree(rule_tree) # Create the tree (call the function)
+print(RenderTree(mytree).by_attr('id'))
+
+
+
+# def pack_bSDD_prop(prop):
+#     return (prop['propertySet'], prop['name']), prop['dataType'])
+
+
+# def pack_MVD_props(extraction):
+
+
 
 types = get_ifc_types(ifc_file)
 
@@ -54,18 +74,21 @@ built_request = 'https://bs-dd-api-prototype.azurewebsites.net/api/Classificatio
 ifc_bsdd = {}
 
 for t in types:
-    if t == 'IfcWindow':
+    if t == 'IfcWallStandardCase':
+        t = "IfcWall"
+        
         r = requests.get(built_request+ t.lower())
         # ifc_bsdd[t] = json.loads(r.text)
         classification_result = json.loads(r.text)
 
 
-        rule_tree = get_xset_rule(mvd_fn, t, pset)
+        rule_tree = get_xset_rule(mvd_fn, t, qset)
         print(rule_tree)
         for result in mvd.extract_data(rule_tree, ifc_file.by_type(t)[0]):
         # print(result)
             for k,v in result.items():
                 print(k, v)
+            print()
             
         
         if len(classification_result['classificationProperties']):
