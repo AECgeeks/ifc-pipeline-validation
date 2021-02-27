9e66fb5551541e5e5f8c18014f020780e01a5cd7
johltn <j.luttun@laposte.net>
2021-02-27 08:58:18 +0100

    Create log from bSDD check

--- application/checks/check_bSDD.py
+++ application/checks/check_bSDD.py
@@ -9,7 +9,7 @@ from anytree import Node, RenderTree
 
 ifc_fn = "./ifc-python-parser/files/AC20-Institute-Var-2.ifc"
 #ifc_fn = "./ifc-python-parser/files/test_file.ifc"
-#ifc_fn = "./ifc-python-parser/files/Duplex_A_20110505.ifc"
+ifc_fn = "./ifc-python-parser/files/Duplex_A_20110505.ifc"
 
 ifc_file = ifcopenshell.open(ifc_fn)
 
@@ -45,7 +45,7 @@ def pack_classification(classification_props):
     return classification_props['propertySet'], classification_props['name'], classification_props['dataType'] 
 
 def pack_mvd(mvd_output):
-    return list(mvd_output.values())[0], list(mvd_output.values())[1], list(mvd_output.values())[3]
+    return list(mvd_output.values())[0], list(mvd_output.values())[1], list(mvd_output.values())[3].wrappedValue
 
 simple_type_python_mapping = {
     # @todo should include unicode for Python2
@@ -70,8 +70,11 @@ types = get_ifc_types(ifc_file)
 built_request = 'https://bs-dd-api-prototype.azurewebsites.net/api/Classification/v2?namespaceUri=http%3A%2F%2Fidentifier.buildingsmart.org%2Furi%2Fbuildingsmart%2Fifc-4.3%2Fclass%2F'
 ifc_bsdd = {}
 
+log_to_construct = {}
+
 for t in types:
     print(t)
+    log_to_construct[t] = {}
     # t = "IfcDoor"
     r = requests.get(built_request+ t.lower())
     # ifc_bsdd[t] = json.loads(r.text)
@@ -81,31 +84,40 @@ for t in types:
     # print(rule_tree)
     
     if 'classificationProperties' in classification_result.keys():
-        print(len(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])))
-        d = list(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0]))[0]
-        val = list(d.values())[0]
+        # print(len(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])))
+        # d = list(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0]))[0]
+        # val = list(d.values())[0]
 
         packed_properties = [pack_classification(p) for p in classification_result['classificationProperties'] if not p['propertySet'].startswith("Qto")]
         checking = {el:"Not present" for el in packed_properties}
         # if not len(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])) == 1 and val =='empty data structure':
-          
-        if len(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])) > 1:
-            packed_output = [pack_mvd(d) for d in mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])]
-            to_compare = [packed_output, packed_properties]
+        for instance in ifc_file.by_type(t):
+            print("    ", instance.GlobalId)
+            log_to_construct[t][instance.GlobalId] = []
+            if len(mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])) > 1:
+                packed_output = [pack_mvd(d) for d in mvd.extract_data(rule_tree, ifc_file.by_type(t)[0])]
+                to_compare = [packed_output, packed_properties]
 
-            for element in itertools.product(*to_compare):
-                match = element[0][0] == element[1][0] and element[0][1] == element[1][1]
-                if match:
-                    if isinstance(element[0][2].wrappedValue,simple_type_python_mapping[element[1][2]]):
-                        # print(element[1], 'Property validated')
-                        checking[element[1]] = element[0]
-                    else:
-                        checking[element[1]] = 'wrong type'
+                for element in itertools.product(*to_compare):
+                    match = element[0][0] == element[1][0] and element[0][1] == element[1][1]
+                    if match:
+                        if isinstance(element[0][2],simple_type_python_mapping[element[1][2]]):
+                            # print(element[1], 'Property validated')
+                            checking[element[1]] = element[0]
+                        else:
+                            checking[element[1]] = 'wrong type'
 
-            for ck,cv in checking.items():
-                print("        ", ck, cv)
+                for ck,cv in checking.items():
+                    print("          ", ck, cv)
+                    log_to_construct[t][instance.GlobalId].append((ck, cv,))
+            
 
-                        
+
+
+print(log_to_construct)      
+
+with open('ogs/bsddlog.json', 'w', encoding='utf-8') as f:
+    json.dump(log_to_construct, f, ensure_ascii=False, indent=4)
 
 
 
