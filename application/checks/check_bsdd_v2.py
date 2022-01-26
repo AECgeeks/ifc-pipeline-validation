590a59028321099aa2a0aa5ee0c17f8fbe2157c6
johltn <luttun.j@gmail.com>
2022-01-26 11:20:42 -0500

    Use context manager for DB session

--- application/checks/check_bsdd_v2.py
+++ application/checks/check_bsdd_v2.py
@@ -4,6 +4,7 @@ import requests
 import json
 import numpy as np
 import argparse
+from helper import database
 
 
 
@@ -72,115 +73,89 @@ def validate_instance(constraint,ifc_file, instance):
 
 
 def check_bsdd(ifc_fn, USE_DB, task_id):
-
-    if USE_DB:
-        from helper import database
-
+   
     file_code = ifc_fn.split(".ifc")[0]
     ifc_file = ifcopenshell.open(ifc_fn)
-
-    if USE_DB:
-        session = database.Session()
-
+  
+    with database.Session() as session:
         model = session.query(database.model).filter(database.model.code == file_code)[0]
         file_id = model.id
-        session.close()
-
         
-    n = len(ifc_file.by_type("IfcRelAssociatesClassification"))
+        n = len(ifc_file.by_type("IfcRelAssociatesClassification"))
 
-    if n:
-        rnd_array = np.random.multinomial(100, np.ones(n)/n, size=1)[0]
-        for idx, rel in enumerate(ifc_file.by_type("IfcRelAssociatesClassification")):
+        if n:
+            rnd_array = np.random.multinomial(100, np.ones(n)/n, size=1)[0]
+            for idx, rel in enumerate(ifc_file.by_type("IfcRelAssociatesClassification")):
 
-            sys.stdout.write(rnd_array[idx] * ".")
-            sys.stdout.flush()
+                sys.stdout.write(rnd_array[idx] * ".")
+                sys.stdout.flush()
 
+                related_objects = rel.RelatedObjects
+                relating_classification = rel.RelatingClassification
 
-            related_objects = rel.RelatedObjects
-            relating_classification = rel.RelatingClassification
-
-            bsdd_response = validate_ifc_classification_reference(relating_classification)
-            
-            for ifc_instance in related_objects:
-
-                if USE_DB:
-
-                    session = database.Session()
+                bsdd_response = validate_ifc_classification_reference(relating_classification)
+                
+                for ifc_instance in related_objects:
                     instance = database.ifc_instance(ifc_instance.GlobalId, ifc_instance.is_a(), file_id)
                     session.add(instance)
                     session.flush()
                     instance_id = instance.id
-                    session.commit()
-                    session.close()
-                
-                else:
-                    instance_id = ifc_instance.GlobalId
+                    session.commit()               
 
-                # This variable will contain all the information 
-                # about the validation result of an entity instance
-                bsdd_result = {"task_id":0,
-                                    "instance_id":0,
-                                    "bsdd_classification_uri":0,
-                                    "bsdd_property_uri":0,
-                                    "bsdd_property_constraint":0,
-                                    "bsdd_type_constraint":0,
-                                    "ifc_property_set":0,
-                                    "ifc_property_name":0,
-                                    "ifc_property_type":0,
-                                    "ifc_property_value":0
-                                    }
+                    # This variable will contain all the information 
+                    # about the validation result of an entity instance
+                    bsdd_result = {"task_id":0,
+                                        "instance_id":0,
+                                        "bsdd_classification_uri":0,
+                                        "bsdd_property_uri":0,
+                                        "bsdd_property_constraint":0,
+                                        "bsdd_type_constraint":0,
+                                        "ifc_property_set":0,
+                                        "ifc_property_name":0,
+                                        "ifc_property_type":0,
+                                        "ifc_property_value":0
+                                        }
 
-                if bsdd_response:
-                    if has_specifications(json.loads(bsdd_response.text)):
-                        specifications = json.loads(bsdd_response.text)["classificationProperties"]
-                        for constraint in specifications:
+                    if bsdd_response:
+                        if has_specifications(json.loads(bsdd_response.text)):
+                            specifications = json.loads(bsdd_response.text)["classificationProperties"]
+                            for constraint in specifications:
 
+                                # Validation of the instance
+                                constraint_content = json.loads(bsdd_response.text)
 
-                            # Validation of the instance
-                            
-                            constraint_content = json.loads(bsdd_response.text)
+                                # Store everything in DB
+                                bsdd_result["task_id"] = task_id
+                                
+                                # Should create instance entry
+                                bsdd_result["instance_id"] = instance_id
 
-                            # Store everything in DB
-                            bsdd_result["task_id"] = task_id
-                            
-                            # Should create instance entry
-                            bsdd_result["instance_id"] = instance_id
+                                bsdd_result["bsdd_classification_uri"] = constraint_content["namespaceUri"]
+                                bsdd_result["bsdd_type_constraint"] = ";".join(constraint_content["relatedIfcEntityNames"])
+                                bsdd_result["bsdd_property_constraint"] = json.dumps(constraint)
+                                bsdd_result["bsdd_property_uri"] = constraint["propertyNamespaceUri"]
 
-                            bsdd_result["bsdd_classification_uri"] = constraint_content["namespaceUri"]
-                            bsdd_result["bsdd_type_constraint"] = ";".join(constraint_content["relatedIfcEntityNames"])
-                            bsdd_result["bsdd_property_constraint"] = json.dumps(constraint)
-                            bsdd_result["bsdd_property_uri"] = constraint["propertyNamespaceUri"]
+                                results = validate_instance(constraint, ifc_file, ifc_instance)
 
-                            results = validate_instance(constraint, ifc_file, ifc_instance)
-
-
-                            bsdd_result["ifc_property_set"] = results["result"]["pset_name"]
-                            bsdd_result["ifc_property_name"] = results["result"]["property_name"]
-                            
-                            if not isinstance(results["result"]["datatype"], str):
-                                bsdd_result["ifc_property_type"] = results["result"]["datatype"].__name__
-                            bsdd_result["ifc_property_value"] = results["result"]["value"]
-                            
-
-                            if USE_DB:
-                                session = database.Session()
+                                bsdd_result["ifc_property_set"] = results["result"]["pset_name"]
+                                bsdd_result["ifc_property_name"] = results["result"]["property_name"]
+                                
+                                if not isinstance(results["result"]["datatype"], str):
+                                    bsdd_result["ifc_property_type"] = results["result"]["datatype"].__name__
+                                bsdd_result["ifc_property_value"] = results["result"]["value"]
+                                
                                 db_bsdd_result = database.bsdd_result(bsdd_result["task_id"])
 
                                 for key, value in bsdd_result.items():
                                     setattr(db_bsdd_result, key, value) 
                                 
                                 session.add(db_bsdd_result)
-                                
                                 session.commit()
-                                session.close()
-
-                            pass
-                    else:
-                        # Record NULL in other fields
-                        bsdd_result["bsdd_property_constraint"] = "no constraint"
-                        if USE_DB:  
-                            session = database.Session()
+                                pass
+                        else:
+                            # Record NULL in other fields
+                            bsdd_result["bsdd_property_constraint"] = "no constraint"
+                            
                             db_bsdd_result = database.bsdd_result(bsdd_result["task_id"])
 
                             for key, value in bsdd_result.items():
@@ -188,39 +163,30 @@ def check_bsdd(ifc_fn, USE_DB, task_id):
 
                             session.add(db_bsdd_result)
                             session.commit()
-                            session.close()
-
-                        pass
-                else:
-                    # Record NULL everywhere in bsdd_result
-                    bsdd_result["bsdd_classification_uri"] = "classification not found"
-                    
-                    if USE_DB:
-                        session = database.Session()
+                    else:
+                        # Record NULL everywhere in bsdd_result
+                        bsdd_result["bsdd_classification_uri"] = "classification not found"
+                        
                         db_bsdd_result = database.bsdd_result(bsdd_result["task_id"])
 
                         for key, value in bsdd_result.items():
                             setattr(db_bsdd_result, key, value) 
 
-
                         session.add(db_bsdd_result)
                         session.commit()
-                        session.close()
-                        pass
-
-    if USE_DB:
+                    
+    
         #todo: implement scores that actually validate or not the model
-        session = database.Session()
         model = session.query(database.model).filter(database.model.code == file_code)[0]
         model.status_bsdd = 'v'
         session.commit()
         session.close()
-        
+
 
 if __name__=="__main__":
         parser = argparse.ArgumentParser(description="Generate classified IFC file")
         parser.add_argument("--input","-i", default="Duplex_A_20110505.ifc", type=str)
-        parser.add_argument("--db","-d", default=0, type=bool)
+        parser.add_argument("--db","-d", default=1, type=bool)
         parser.add_argument("--task","-t", default=0, type=int)
 
         args = parser.parse_args()
