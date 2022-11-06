fa3be6a09d4699b179b2b3679e28618a554c00e7
johltn <luttun.j@gmail.com>
2022-11-06 17:34:43 -0500

    Add dropzone in reapage

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Dz from './Dz'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Dz/>
     </div>
   );
 }
---
+++ ui/src/Dz.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+
+function Dz() {
+
+    useEffect(() => {
+       
+        window.Dropzone.autoDiscover = false;
+        var dz = new window.Dropzone("#ifc_dropzone",
+            {
+                uploadMultiple: true,
+                acceptedFiles: ".ifc, .xml",
+                parallelUploads: 100,
+                maxFiles: 100,
+                maxFilesize: 8 * 1024,
+                autoProcessQueue: false,
+                addRemoveLinks: true,
+            });
+
+        dz.on("addedfile", file => { console.log("new file") });
+
+
+        dz.on("success", function (file, response) {
+            window.location = response.url;
+
+        });
+
+        var submitButton = document.querySelector("#submit");
+        submitButton.addEventListener("click", function () {
+            dz.processQueue();
+
+        });
+
+
+    }, []);
+
+    return (
+        <div>
+            <div className="submit-area" id="ifc_tab">
+                <form action="http://localhost:5000/" className="dropzone" id="ifc_dropzone">
+                    <div className="dz-message" data-dz-message><span><i className="material-icons">file_upload</i> Click or drop files here to upload for validation</span></div>
+                </form>
+                <button className="submit-button" id="submit">Start validation</button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Dz;
